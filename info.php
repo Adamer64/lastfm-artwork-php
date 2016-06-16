d33142d730201b27b01272e6a9c7532cb7502441
Adamer64 <mesarc.adam@gmail.com>
2016-06-17 00:22:03 +0200

    Update info.php

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 require_once("lastfm/stream.php");
-echo strtoupper($info['artist']); // Echo artist's name in uppercase
+echo strtoupper($info['artist']); // Echo artists name in uppercase
 echo "</br>";
 echo $info['track']; // Echo track name
 ?>
